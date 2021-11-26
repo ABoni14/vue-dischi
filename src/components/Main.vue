0a58e76339e1b0512e81e4f4a02b321950b39dad
ABoni14 <aboni150@gmail.com>
2021-11-26 14:39:52 +0100

    main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,43 @@
 <template>
   <main>
-
+    <div class="container main-container">
+      <ProductBox 
+        v-for="(album, index) in albums"
+        :key="index"
+        :albums="album"/>
+    </div>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+import ProductBox from "./ProductBox.vue";
+
 export default {
-  name: "Main"
+  name: "Main",
+  components: {
+    ProductBox
+  },
+  data(){
+    return{
+      albums: [],
+      apiUrl: "https://flynn.boolean.careers/exercises/api/array/music"
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+        .then( r => {
+          this.albums = r.data.response;
+        })
+        .catch( e => {
+          console.log(e);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
 
@@ -16,6 +47,10 @@ export default {
 main{
   height: calc(100vh - 100px);
   background-color: $secondary-color;
+  .main-container{
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 40px;
+  }
 }
-
 </style>
---
+++ src/components/ProductBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <img :src="albums.poster" :alt="albums.title">
+    <h3>{{albums.title}}</h3>
+    <h5>{{albums.author}}</h5>
+    <h5>{{albums.year}}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductBox",
+  props:{
+    albums: Object
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/style/vars.scss";
+
+.box{
+  width: calc(100% / 5 - 30px);
+  margin: 15px;
+  background-color: $primary-color;
+  text-align: center;
+  padding-bottom: 50px;
+  padding: 15px;
+  img{
+    width: 100%;
+    margin-bottom: 15px;
+  }
+  h3{
+    color: white;
+    margin-bottom: 10px;
+    text-transform: uppercase;
+  }
+  h5{
+    font-size: 15px;
+    color: grey;
+  }
+}
+</style>
